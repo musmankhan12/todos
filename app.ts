8512be241332e1fcbb93ca5666a234ce9b7dfc2d
musmankhan12 <159324068+musmankhan12@users.noreply.github.com>
2024-03-30 14:53:14 -0700

    todos
    cli-todo plz check

---
+++ app.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+// user variable//
+let todos = [];
+ let condition = true;
+while(condition)
+{
+//open loop//
+    let userinput = await inquirer.prompt
+    ([
+        {
+           name:"TodoItem", type:"input", message:"What you want to Add Todo Item in listing:",
+        },
+      
+        {   
+           name:"AddMore", type:"confirm", message: "Do you want to AddMore:", 
+        }
+    ]);
+ //output of program
+                                                            todos.push(userinput.TodoItem);
+                                                           condition = userinput.AddMore
+                                                           console.log("todos:");}
+
+//Add More items//
+for (let i= 0; i<todos.length;i++)
+{console.log(todos[i])}
+
+//Exit loop//
